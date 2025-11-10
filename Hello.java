58320fa37aa9969090618891cb4aab30b613a412
Faiz-Sahil99 <sahilfaiz254@gamil.com>
2025-11-10 12:30:34 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,46 @@
+ 
+import java.util.*; 
+
+public class Hello{
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner (System.in);
+    int button = sc. nextInt();
+
+
+   
+    /* 
+
+    if(a ==b ){
+      System.out.println("Equal");
+    } else if (a >b ){
+      System.out.println("a is greater");
+
+    }
+  else{
+    System.out.println("a is lesser");
+    sc.close();
+  }
+   */
+switch(button){
+    case 1 : System.out.println("hello");
+    break;
+    case 2 : System.out.println("namaste");
+    break;
+    case 3 : System.out.println("bonjour");
+    break;
+    default:System.out.println("invalid button");
+    sc.close();
+
+
+   }   
+  }
+}
+
+    
+       
+     
+
+
+
+
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] array){
+        System.out.println("hello faiz");
+    }
+} 
+    
+
